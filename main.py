d1701ef3cf98a36ef4a7176db4be4f6bc368e481
Kai Hiwatari <hemantjeetu169@gmail.com>
2022-10-15 02:02:47 +0000

    Update

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import tkinter 
 from tkinter import *
+import sys
+import os
 
+if os.environ.get('DISPLAY','') == '':
+    print('no display found. Using :0.0')
+    os.environ.__setitem__('DISPLAY', '6080')
 root = Tk()
 root.title('test')
 

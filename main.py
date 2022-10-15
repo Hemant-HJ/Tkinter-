711436f70e28ff472276e81878ef7e07a5ddb09d
Kai Hiwatari <hemantjeetu169@gmail.com>
2022-10-15 05:32:40 +0000

    updated

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import sys
 import os
 
 if os.environ.get('DISPLAY','') == '':
-    print('no display found. Using :0.0')
     os.environ.__setitem__('DISPLAY', '6080')
+
 root = Tk()
 root.title('test')
 
